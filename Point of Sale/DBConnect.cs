b9f6abcbfef3150951d51c80c273ccffd97198b3
Drumarcco <marcoantonioam9@gmail.com>
2015-03-01 00:56:33 -0700

    Add DBConnect

---
+++ Point of Sale/DBConnect.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace Point_of_Sale
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+
+        public DBConnect(User user)
+        {
+            Initialize(user);
+            this.OpenConnection();
+        }
+
+        private void Initialize(User user)
+        {
+            server = "localhost";
+            database = "point-of-sale";
+            uid = user.Username;
+            password = user.Password;
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+                database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        private bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                MessageBox.Show("Opened");
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        MessageBox.Show("No se puede conectar al servidor. Contacte al administrador.");
+                        break;
+                    case 1045:
+                        MessageBox.Show("Usuario y contraseña invalidos, intente de nuevo.");
+                        break;
+                }
+                return false;
+            }
+        }
+
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+        }
+    }
+}
