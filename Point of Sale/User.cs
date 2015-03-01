81d673f2b9e5362a898a95d6a6fdd6eb7c315733
Drumarcco <marcoantonioam9@gmail.com>
2015-03-01 00:58:03 -0700

    Add username property to user

--- Point of Sale/User.cs
+++ Point of Sale/User.cs
@@ -10,6 +10,7 @@ namespace Point_of_Sale
     {
         public string Name { get; set; }
         public int Id { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
 
     }
