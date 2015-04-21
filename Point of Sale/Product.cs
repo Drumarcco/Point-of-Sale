c54da408d341a1d27d7ef28473e51098777d7c25
Drumarcco <marcoantonioam9@gmail.com>
2015-04-20 21:24:38 -0700

    New method to get product by ID from DB

--- Point of Sale/Product.cs
+++ Point of Sale/Product.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Point_of_Sale
 {
@@ -15,6 +18,37 @@ namespace Point_of_Sale
         public float Cost { get; set; }
         public float Price { get; set; }
         public int Provider { get; set; }
+
+        public Product() { 
         
+        }
+
+        public Product(int productID) {
+            MySqlConnection connection = new MySqlConnection(DBConnect.ConnectionString);
+            try
+            {
+                MySqlCommand command = new MySqlCommand("SELECT * FROM product WHERE id=@id", connection);
+                command.Parameters.AddWithValue("@id", productID);
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                DataTable productTable = new DataTable();
+                adapter.Fill(productTable);
+
+                DataRow productInfo = productTable.Rows[0];
+                this.Id = productID;
+                this.Name = (String)productInfo["Name"];
+                this.Price = (float)productInfo["Price"];
+                this.Provider = (int)productInfo["Provider"];
+                this.Category = (int)productInfo["Category"];
+                this.Cost = (float)productInfo["Cost"];
+                this.DiscountPercent = (int)productInfo["DiscountPercent"];
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                if (connection != null) connection.Close();
+            }
+        }
     }
 }
