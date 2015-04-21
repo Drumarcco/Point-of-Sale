269c5664810f51ffbf5114f773c00ccddaeaaee3
Drumarcco <marcoantonioam9@gmail.com>
2015-04-20 21:23:55 -0700

    Updated search UI

--- Point of Sale/FormHome.Designer.cs
+++ Point of Sale/FormHome.Designer.cs
@@ -28,19 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txt_Search = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // txt_Search
+            // 
+            this.txt_Search.Location = new System.Drawing.Point(77, 15);
+            this.txt_Search.Name = "txt_Search";
+            this.txt_Search.Size = new System.Drawing.Size(371, 20);
+            this.txt_Search.TabIndex = 0;
+            this.txt_Search.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_Search_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Búsqueda:";
             // 
             // FormHome
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(465, 417);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Search);
             this.Name = "FormHome";
             this.Text = "Inicio";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txt_Search;
+        private System.Windows.Forms.Label label1;
     }
 }
 
