e46b94146a4605ebe75b0a9777c1703229324fc1
Drumarcco <marcoantonioam9@gmail.com>
2015-04-21 18:43:55 -0700

    Adds product list sale visualization.

--- Point of Sale/FormHome.Designer.cs
+++ Point of Sale/FormHome.Designer.cs
@@ -28,15 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
             this.txt_Search = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.dag_productTable = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Product = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Discount = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Subtotal = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dag_productTable)).BeginInit();
             this.SuspendLayout();
             // 
             // txt_Search
             // 
             this.txt_Search.Location = new System.Drawing.Point(77, 15);
             this.txt_Search.Name = "txt_Search";
-            this.txt_Search.Size = new System.Drawing.Size(371, 20);
+            this.txt_Search.Size = new System.Drawing.Size(483, 20);
             this.txt_Search.TabIndex = 0;
             this.txt_Search.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_Search_KeyDown);
             // 
@@ -49,14 +61,74 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Búsqueda:";
             // 
+            // dag_productTable
+            // 
+            this.dag_productTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dag_productTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.Product,
+            this.Quantity,
+            this.Price,
+            this.Discount,
+            this.Subtotal});
+            this.dag_productTable.Location = new System.Drawing.Point(16, 59);
+            this.dag_productTable.Name = "dag_productTable";
+            this.dag_productTable.Size = new System.Drawing.Size(544, 150);
+            this.dag_productTable.TabIndex = 2;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "Id";
+            this.ID.Name = "ID";
+            this.ID.Visible = false;
+            // 
+            // Product
+            // 
+            this.Product.HeaderText = "Producto";
+            this.Product.Name = "Product";
+            // 
+            // Quantity
+            // 
+            dataGridViewCellStyle5.Format = "N2";
+            dataGridViewCellStyle5.NullValue = null;
+            this.Quantity.DefaultCellStyle = dataGridViewCellStyle5;
+            this.Quantity.HeaderText = "Cantidad";
+            this.Quantity.Name = "Quantity";
+            // 
+            // Price
+            // 
+            dataGridViewCellStyle6.Format = "C2";
+            dataGridViewCellStyle6.NullValue = null;
+            this.Price.DefaultCellStyle = dataGridViewCellStyle6;
+            this.Price.HeaderText = "Precio Unitario";
+            this.Price.Name = "Price";
+            // 
+            // Discount
+            // 
+            dataGridViewCellStyle7.Format = "C2";
+            dataGridViewCellStyle7.NullValue = null;
+            this.Discount.DefaultCellStyle = dataGridViewCellStyle7;
+            this.Discount.HeaderText = "Descuento";
+            this.Discount.Name = "Discount";
+            // 
+            // Subtotal
+            // 
+            dataGridViewCellStyle8.Format = "C2";
+            dataGridViewCellStyle8.NullValue = null;
+            this.Subtotal.DefaultCellStyle = dataGridViewCellStyle8;
+            this.Subtotal.HeaderText = "Importe";
+            this.Subtotal.Name = "Subtotal";
+            // 
             // FormHome
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
-            this.ClientSize = new System.Drawing.Size(465, 417);
+            this.ClientSize = new System.Drawing.Size(786, 417);
+            this.Controls.Add(this.dag_productTable);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txt_Search);
             this.Name = "FormHome";
             this.Text = "Inicio";
+            ((System.ComponentModel.ISupportInitialize)(this.dag_productTable)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,6 +138,13 @@
 
         private System.Windows.Forms.TextBox txt_Search;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dag_productTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Product;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Discount;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Subtotal;
     }
 }
 
--- Point of Sale/FormHome.cs
+++ Point of Sale/FormHome.cs
@@ -14,11 +14,11 @@ namespace Point_of_Sale
 {
     public partial class FormHome : Form
     {
-        private List<Product> productList;
+        private BindingList<Lot> productList;
         public FormHome()
         {
             InitializeComponent();
-            productList = new List<Product>();
+            productList = new BindingList<Lot>();
         }
 
         private void txt_Search_KeyDown(object sender, KeyEventArgs e)
@@ -29,22 +29,75 @@ namespace Point_of_Sale
                 {
                     DialogProductSearch productSearch = new DialogProductSearch(txt_Search.Text);
                     productSearch.ShowDialog();
-                    if (productSearch.DialogResult == DialogResult.OK) {
+                    if (productSearch.DialogResult == DialogResult.OK)
+                    {
                         productID = productSearch.productId;
                     }
+                    else {
+                        txt_Search.Clear();
+                        return;
+                    }
                 }
-                Product product = new Product(productID);
-                addProductToList(product);
+                addProductToList(productID);
+                txt_Search.Clear();
             }
         }
 
-        private void addProductToList(Product product) {
-            productList.Add(product);
-            updateProductListTable();
+        private void addProductToList(int productID) {
+            try
+            {
+                Product product = new Product(productID);
+                if (isProductOnList(product))
+                {
+                    int lotIndex = getLotIndexFromList(product);
+                    Lot lot = productList[lotIndex];
+                    lot.Quantity++;
+                    productList[lotIndex] = lot;
+                }
+                else
+                {
+                    Lot lot = new Lot(product, 1);
+                    productList.Add(lot);
+                }
+                updateProductListTable();
+            }
+            catch (ArgumentException argEx) {
+                MessageBox.Show(argEx.Message);
+            }
+            
         }
 
+        private bool isProductOnList(Product product) {
+            foreach (Lot lot in productList) {
+                if (lot.Product.Id == product.Id) return true;
+            }
+            return false;
+        }
+
+        private int getLotIndexFromList(Product product) { 
+            int i = 0;
+            foreach (Lot lot in productList) {
+                if (lot.Product.Id == product.Id)
+                {
+                    return i;
+                }
+                i++;
+            }
+            return -1;
+        }
+
+
         private void updateProductListTable() {
-            throw new NotImplementedException();
+            dag_productTable.Rows.Clear();
+            foreach (Lot lot in productList) {
+                DataGridViewRow row = (DataGridViewRow)dag_productTable.Rows[0].Clone();
+                row.Cells[1].Value = lot.Product.Name;
+                row.Cells[2].Value = lot.Quantity;
+                row.Cells[3].Value = lot.Product.getRealPrice();
+                row.Cells[4].Value = lot.Product.getDiscount();
+                row.Cells[5].Value = lot.Subtotal;
+                dag_productTable.Rows.Add(row);
+            }
         }
     }
 }
