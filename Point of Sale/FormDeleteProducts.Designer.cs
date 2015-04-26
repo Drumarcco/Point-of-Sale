8ffb54ad17b135e6373a18fc76670a78d3482410
Drumarcco <marcoantonioam9@gmail.com>
2015-04-25 17:19:36 -0700

    Delete products

---
+++ Point of Sale/FormDeleteProducts.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Point_of_Sale
+{
+    partial class FormDeleteProducts
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Delete = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.txt_ID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID de Producto";
+            // 
+            // btn_Delete
+            // 
+            this.btn_Delete.Location = new System.Drawing.Point(16, 72);
+            this.btn_Delete.Name = "btn_Delete";
+            this.btn_Delete.Size = new System.Drawing.Size(75, 23);
+            this.btn_Delete.TabIndex = 1;
+            this.btn_Delete.Text = "Eliminar";
+            this.btn_Delete.UseVisualStyleBackColor = true;
+            this.btn_Delete.Click += new System.EventHandler(this.btn_Delete_Click);
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(131, 72);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_cancel.TabIndex = 2;
+            this.btn_cancel.Text = "Cancelar";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // txt_ID
+            // 
+            this.txt_ID.Location = new System.Drawing.Point(98, 20);
+            this.txt_ID.Name = "txt_ID";
+            this.txt_ID.Size = new System.Drawing.Size(108, 20);
+            this.txt_ID.TabIndex = 3;
+            // 
+            // FormDeleteProducts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(239, 110);
+            this.Controls.Add(this.txt_ID);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.btn_Delete);
+            this.Controls.Add(this.label1);
+            this.Name = "FormDeleteProducts";
+            this.Text = "FormDeleteProducts";
+            this.Load += new System.EventHandler(this.FormDeleteProducts_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Delete;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.TextBox txt_ID;
+    }
+}
---
+++ Point of Sale/FormDeleteProducts.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Point_of_Sale
+{
+    public partial class FormDeleteProducts : Form
+    {
+        public FormDeleteProducts()
+        {
+            InitializeComponent();
+        }
+
+        private void FormDeleteProducts_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_Delete_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection(DBConnect.ConnectionString);
+            try
+            {
+                connection.Open();
+
+                MySqlCommand cmd = new MySqlCommand("delete_product", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                int anInteger;
+                anInteger = Convert.ToInt32(txt_ID.Text);
+                cmd.Parameters.AddWithValue("@ident", anInteger);
+
+                cmd.ExecuteNonQuery();
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
