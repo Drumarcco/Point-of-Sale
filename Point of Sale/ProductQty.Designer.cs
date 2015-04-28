67e4a33423f49d34751a7a2aa49479ba79a1f49b
Arturo Martinez <mtzviesca03@gmail.com>
2015-04-27 22:42:47 -0700

    Insertar y Eliminar proveedores - Borrado de prod

---
+++ Point of Sale/ProductQty.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Point_of_Sale
+{
+    partial class ProductQty
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_Qty = new System.Windows.Forms.TextBox();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cantidad de Producto:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txt_Qty
+            // 
+            this.txt_Qty.Location = new System.Drawing.Point(131, 6);
+            this.txt_Qty.Name = "txt_Qty";
+            this.txt_Qty.Size = new System.Drawing.Size(66, 20);
+            this.txt_Qty.TabIndex = 1;
+            this.txt_Qty.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txt_Qty_KeyDown);
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.Location = new System.Drawing.Point(79, 39);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(75, 23);
+            this.btn_ok.TabIndex = 2;
+            this.btn_ok.Text = "Aceptar";
+            this.btn_ok.UseVisualStyleBackColor = true;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // ProductQty
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(220, 74);
+            this.Controls.Add(this.btn_ok);
+            this.Controls.Add(this.txt_Qty);
+            this.Controls.Add(this.label1);
+            this.Name = "ProductQty";
+            this.Text = "ProductQty";
+            this.Load += new System.EventHandler(this.ProductQty_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ProductQty_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Qty;
+        private System.Windows.Forms.Button btn_ok;
+    }
+}
