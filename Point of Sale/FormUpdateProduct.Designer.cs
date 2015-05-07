9cab330c2c4b379b61acfe1ca1ee312a320bc06e
Vaztor29 <jufravazto@hotmail.com>
2015-05-06 19:57:09 -0700

    FormAddDeleteInventory, Class Inventory, Consultas a Procedimientos ("insert_receopt" y "delete_issues"),ContextMenu Agregado
    Se creo la Pantalla de Agregado y Eliminado de Producto al Inventario,
    Clase Inventario, Menu Context (Agregar a Inventairo y Eliminar de
    Inventario al hacer click derecho en la forma "UpdateProduct").

--- Point of Sale/FormUpdateProduct.Designer.cs
+++ Point of Sale/FormUpdateProduct.Designer.cs
@@ -41,10 +41,14 @@
             this._point_of_saleDataSet = new Point_of_Sale._point_of_saleDataSet();
             this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.productTableAdapter = new Point_of_Sale._point_of_saleDataSetTableAdapters.productTableAdapter();
+            this.cms_MouseClickRight = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.addInventoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteInventoryToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.product_table)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.productBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this._point_of_saleDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
+            this.cms_MouseClickRight.SuspendLayout();
             this.SuspendLayout();
             // 
             // product_table
@@ -66,11 +70,14 @@
             this.product_table.DataSource = this.productBindingSource1;
             this.product_table.Dock = System.Windows.Forms.DockStyle.Fill;
             this.product_table.Location = new System.Drawing.Point(0, 0);
+            this.product_table.Margin = new System.Windows.Forms.Padding(2);
             this.product_table.Name = "product_table";
             this.product_table.ReadOnly = true;
-            this.product_table.Size = new System.Drawing.Size(994, 222);
+            this.product_table.Size = new System.Drawing.Size(746, 180);
             this.product_table.TabIndex = 0;
+            this.product_table.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.product_table_CellClick);
             this.product_table.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.product_table_CellDoubleClick);
+            this.product_table.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.product_table_CellMouseClick);
             // 
             // iDDataGridViewTextBoxColumn
             // 
@@ -140,13 +147,36 @@
             // 
             this.productTableAdapter.ClearBeforeFill = true;
             // 
+            // cms_MouseClickRight
+            // 
+            this.cms_MouseClickRight.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addInventoryToolStripMenuItem,
+            this.deleteInventoryToolStripMenuItem});
+            this.cms_MouseClickRight.Name = "cms_MouseClickRight";
+            this.cms_MouseClickRight.Size = new System.Drawing.Size(190, 48);
+            // 
+            // addInventoryToolStripMenuItem
+            // 
+            this.addInventoryToolStripMenuItem.Name = "addInventoryToolStripMenuItem";
+            this.addInventoryToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.addInventoryToolStripMenuItem.Text = "Agregar a Inventario";
+            this.addInventoryToolStripMenuItem.MouseDown += new System.Windows.Forms.MouseEventHandler(this.addInventoryToolStripMenuItem_MouseDown);
+            // 
+            // deleteInventoryToolStripMenuItem
+            // 
+            this.deleteInventoryToolStripMenuItem.Name = "deleteInventoryToolStripMenuItem";
+            this.deleteInventoryToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.deleteInventoryToolStripMenuItem.Text = "Eliminar de Inventario";
+            this.deleteInventoryToolStripMenuItem.MouseDown += new System.Windows.Forms.MouseEventHandler(this.deleteInventoryToolStripMenuItem_MouseDown);
+            // 
             // FormUpdateProduct
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(994, 222);
+            this.ClientSize = new System.Drawing.Size(746, 180);
             this.Controls.Add(this.product_table);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "FormUpdateProduct";
             this.Text = "FormUpdateProduct";
             this.Load += new System.EventHandler(this.FormUpdateProduct_Load);
@@ -154,6 +184,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.productBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this._point_of_saleDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
+            this.cms_MouseClickRight.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -172,5 +203,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn discountPercentDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn providerDataGridViewTextBoxColumn;
         private System.Windows.Forms.BindingSource productBindingSource1;
+        private System.Windows.Forms.ContextMenuStrip cms_MouseClickRight;
+        private System.Windows.Forms.ToolStripMenuItem addInventoryToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteInventoryToolStripMenuItem;
     }
 }
---
+++ Point of Sale/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point_of_Sale
+{
+    class Inventory
+    {
+        public int Id { get; set; }
+        public float quantity { get; set; }
+        public String nameProduct { get; set; }
+        public bool addordelete { get; set; }
+
+        public Inventory(int Id, String nameProduct, bool addordelete) {
+            this.Id = Id;
+            this.nameProduct = nameProduct;
+            this.addordelete = addordelete;
+        }
+
+    }
+}
