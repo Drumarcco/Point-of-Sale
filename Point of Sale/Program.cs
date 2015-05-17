398b7d45ccafc96c791baf91229b7ce8db95ad06
Drumarcco <marcoantonioam9@gmail.com>
2015-05-16 18:59:17 -0700

    JSReport added to solution, and started on Program.cs

--- Point of Sale/Program.cs
+++ Point of Sale/Program.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using jsreport.Embedded;
 
 namespace Point_of_Sale
 {
     static class Program
     {
+        public static EmbeddedReportingServer JSReportServer = new EmbeddedReportingServer(port: 2000);
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -21,12 +24,24 @@ namespace Point_of_Sale
             String user = login.savedUser;
             if (result == DialogResult.OK)
             {
+                startJSReportServer();
                 Application.Run(new FormHome(user));
+                stopJSReportServer();
             }
             else
             {
                 Application.Exit();
             }
+        }
+
+        static async Task startJSReportServer() { 
+            await JSReportServer.StartAsync();
+            Console.Write("JSReport Server started");
+        }
+
+        static async Task stopJSReportServer() {
+            await JSReportServer.StopAsync();
+            Console.Write("JSReport Server stopped");
         }
     }
 }
